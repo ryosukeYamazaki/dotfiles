ec79260bcf6cfc2cfdfab2a8a44f89b6218506a3
ryosukeYamazaki <hamilton0826@gmail.com>
2025-07-04 13:59:11 +0900

    :+1: 追記するだけにする

--- .emacs.d/inits/10_org.el
+++ .emacs.d/inits/10_org.el
@@ -1,33 +1,23 @@
 ;;; org-mode --- config
 (setq org-directory "~/Documents/org")
-;(setq org-directory "~/Dropbox/Org")
+
 (setq org-default-notes-file "notes.org")
 ; Org-captureの設定
 
 ; Org-captureを呼び出すキーシーケンス
 (define-key global-map "\C-cc" 'org-capture)
-                                        ; Org-captureのテンプレート（メニュー）の設定
-(setq org-capture-templates
-        `(("j" "Journal Entry" entry ; "j" がキャプチャのキー
-           ;; ファイルの場所を org-journal の設定と動的に同期させる
-           (file+datetree (lambda () (format-time-string org-journal-file-format)) "Notes")
-           "* %?\n  %i" ; キャプチャする内容のテンプレート
-           :empty-lines-before 1
-           :empty-lines-after 1)))
-
-(setq org-log-done 'time)
 
 (use-package org-journal
   :ensure t
   :defer t  ; Emacs起動を高速化するため、必要になるまで読み込みを遅延
   :init
   ;; ジャーナルファイルを保存するディレクトリを指定
-  (setq org-journal-dir "~/org/journal/")
+  (setq org-journal-dir "~/Documents/org/journal/")
   ;; ファイル名のフォーマットを指定（例: 2025-07-04.org）
   (setq org-journal-file-format "%Y-%m-%d.org")
   ;; 新しいファイルを作成した際のテンプレート
   (setq org-journal-new-entry-template
-        (string
+        (concat
          "#+title: %<%Y-%m-%d %A>\n\n" ; タイトルに日付と曜日を入れる
          "* Tasks\n\n"
          "* Notes\n\n"))
@@ -36,3 +26,16 @@
   ;; おすすめのキーバインド（"C-c n j" -> note journal のような覚え方）
   ("C-c n j" . org-journal-new-entry))
 
+(setq org-capture-templates
+        `(("j" "Journal Entry"; "j" がキャプチャのキー
+           ;; ファイルの場所を org-journal の設定と動的に同期させる
+           entry (file
+                  (lambda ()
+                    (expand-file-name ; パスを絶対パスに変換
+                     (format-time-string org-journal-file-format) ; ファイル名を生成
+                     org-journal-dir)) ; org-journalのディレクトリを指定(lambda () (format-time-string org-journal-file-format))
+                  )
+           "** \n:PROPERTIES:\n:CREATED: %T\n:END:\n%i\n %a" ; キャプチャする内容のテンプレート
+           :empty-lines 1)))
+
+(setq org-log-done 'time)
